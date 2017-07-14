f4156e605259fcc6640361d2a8a7cdc4998549f3
misteraj <ajlongstreet@gmail.com>
2017-07-14 17:02:57 -0400

    adding search

--- worklog.py
+++ worklog.py
@@ -19,7 +19,7 @@ def menuloop():
             add_entry()
         if toggle == 'v':
             view_entries()
-        if toggle == 'b':
+        if toggle == 's':
             search_loop()
 
 
@@ -43,24 +43,28 @@ def view_entries():
 
 
 def by_date(search_string):
+    '''Search by Date'''
     for item in task_list:
         print(item.find_by_date(search_string))
     pass
 
 
 def by_time(search_string):
+    '''Search by Time'''
     for item in task_list:
         print(item.find_by_time(search_string))
     pass
 
 
 def by_exact(search_string):
+    '''Search by Exact phrase'''
     for item in task_list:
         print(item.find_by_exact(search_string))
     pass
 
 
 def by_pattern(search_string):
+    '''Search by Regex pattern'''
     for item in task_list:
         print(item.find_by_pattern(search_string))
     pass
@@ -71,7 +75,7 @@ def search_loop():
     toggle = None
     while toggle != 'b':
         print('')
-        for key, value in menu.items():
+        for key, value in search_menu.items():
             print('{}) {}'.format(key, value.__doc__))
         toggle = input('\nMake a selection '
                        '(b to return to the main menu): ').lower().strip()
@@ -84,6 +88,12 @@ def search_loop():
         if toggle == 'p':
             by_pattern(input('Please enter a regex pattern'))
 
+search_menu = OrderedDict([
+    ('d', by_date),
+    ('t', by_time),
+    ('e', by_exact),
+    ('p', by_pattern)
+])
 
 menu = OrderedDict([
     ('a', add_entry),
